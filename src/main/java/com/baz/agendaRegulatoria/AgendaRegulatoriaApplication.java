dde8885924198e5404a9cf389446a19706561139
Miguel Mexicano Herrera <miguelmexicano18@gmail.com>
2023-07-13 10:59:56 -0600

    first commit

---
+++ src/main/java/com/baz/agendaRegulatoria/AgendaRegulatoriaApplication.java
@@ -0,0 +1,13 @@
+package com.baz.agendaRegulatoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaRegulatoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaRegulatoriaApplication.class, args);
+	}
+
+}
